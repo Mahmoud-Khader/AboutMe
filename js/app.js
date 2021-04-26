7777d8ab9a5404a36937bd676f4d3a480a652063
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-04-26 16:28:01 +0300

    Changed the Q

--- js/app.js
+++ js/app.js
@@ -5,79 +5,79 @@ let userName = prompt('Could you enter your name please');
 alert(' Hello Mr/Ms ' + userName);
 
 // eslint-disable-next-line no-unused-vars
-let userInput1 = prompt('Are we in Ramadan Now ?');
+let userInput1 = prompt('Do you know my name ?');
 switch(userInput1.toLowerCase()){
     case 'yes':
     case 'y':   
-        //console.log('you are right we are in ramadan now');
-        alert('you are right we are in ramadan now');
+        //console.log('yes my name is mahmoud');
+        alert('yse my name is mahmoud');
         break;
         case 'no':
         case 'n':    
-        //console.log('you are wrong we are in ramadan now');
-        alert('you are wrong we are in ramadan now');
+        //console.log('my name is mahmoud nice to meet you');
+        alert('my name is mahmoud nice to meet you');
         break;
 }
 
 // eslint-disable-next-line no-unused-vars
-let userInput2 = prompt('Do we have to fast in Ramadan ?');
+let userInput2 = prompt('do i study programming ?');
 switch(userInput2.toLowerCase()){
     case 'yes':
     case 'y':    
-       // console.log('you are right we have to fast in ramadan ');
-        alert('you are right we have to fast in ramadan');
+       // console.log('yes i study programming ');
+        alert('yes i study programming');
         break;
         case 'no':
         case 'n':    
-        //console.log('you are wrong we have to fast in ramadan ');
-        alert('you are wrong we have to fast in ramadan');
+        //console.log('you are wrong i study programming ');
+        alert('you are wrong i study programming');
         break;
 }
 
 
 // eslint-disable-next-line no-unused-vars
-let userInput3 = prompt('Are you tall ?');
+let userInput3 = prompt('is my fav color is red ?');
 switch(userInput3.toLowerCase()){
     case 'yes':
     case 'y':    
-        //console.log('you are tall');
-        alert('you are tall');
+        //console.log('you are right');
+        alert('you are right');
         break;
         case 'no':
         case 'n':    
-        //console.log('you are shorty');
-        alert('you are shorty');
+        //console.log('you are wrong');
+        alert('you are wrong');
         break;
 }
 
 
 // eslint-disable-next-line no-unused-vars
-let userInput4 = prompt('Are you fat ?');
+let userInput4 = prompt('Do i live in Amman  ?');
 switch(userInput4.toLowerCase()){
     case 'yes':
     case 'y':    
-        //console.log('you are fat ');
-        alert('you are fat');
+        //console.log('you are right i live in amman ');
+        alert('you are right i live in amman');
         break;
         case 'no':
         case 'n':    
-        //console.log('you are thin');
-        alert('you are thin');
+        //console.log('you are wrong i live in amman');
+        alert('you are wrong i live in amman');
         break;
 }
 
 // eslint-disable-next-line no-unused-vars
-let userInput5 = prompt('Do you trian ?');
+let userInput5 = prompt('Am i Civil Engineer ?');
 switch(userInput5.toLowerCase()){
     case 'yes':
     case 'y':    
-        //console.log('you are healthy ');
-        alert('you are healthy');
+        //console.log('you are right iam civil engineer ');
+        alert('you are right iam civil engineer');
         break;
         case 'no':
         case 'n':    
-        //console.log('you are Lazy as hell');
-        alert('you are Lazy as hell');
+        //console.log('you are wrong iam civil engineer');
+        alert('you are wrong iam civil engineer');
         break;
 }
 
